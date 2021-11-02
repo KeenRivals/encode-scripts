e529deea4354552a98363f467123acb651cc1708
KeenRivals <KeenRivals@users.noreply.github.com>
2021-11-02 09:29:50 -0400

    Use PowerShell jobs to parallelize instead of workflow. Make file naming more automatic. Av1 as default

--- Parallel Encode.ps1
+++ Parallel Encode.ps1
@@ -1,7 +1,7 @@
-Workflow Encode ($dir){
+function Encode ($dir){
 	$vbitrate='300k'
 	$abitrate='32k'
-	$inFile=(Get-Item $PSCommandPath).Basename ) + ".mkv"
+	$inFile=(Get-Item $PSCommandPath).Basename + ".mkv"
 	$outPrefix= (Get-Item $inFile).Basename
 	$vfilters='fps=30,scale="1280:-1"'
 	$afilters='lowpass=12000,loudnorm=tp=0,aresample=48000'
@@ -11,38 +11,42 @@ Workflow Encode ($dir){
 	
 	$commonParams = "-i `"$($inFile)`"","-y","-b:v $vbitrate","-b:a $abitrate",$afilters,"-ac 1",$vfilters,"-pix_fmt yuv420p","-g 300"
 	
-	Parallel {
-		encode-mp4 $commonParams $outPrefix $dir
-		# encode-vp9 $commonParams $outPrefix $dir
-		encode-av1 $commonParams $outPrefix $dir
+	$encodeMp4 = {
+		param($commonParams, $outPrefix, $dir )
+		Push-Location $dir
+		$logfile = "$env:temp\ffmpeg-mp4-" + (Get-Random) + ".log"
+		$mp4Params = "-passlogfile `"$logfile`"","-profile:v baseline","-preset:v placebo","-movflags +faststart"
+
+		Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $mp4Params + "-pass 1" + "`"$($outPrefix).mp4`"")
+		Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $mp4Params + "-pass 2" + "`"$($outPrefix).mp4`"")
 	}
-}
 
-function encode-mp4 ( $commonParams, $outPrefix, $dir ){
-	Push-Location $dir
-	$logfile = "$env:temp\ffmpeg-mp4-" + (Get-Random) + ".log"
-	$mp4Params = "-passlogfile `"$logfile`"","-profile:v baseline","-preset:v placebo"
+	$encodeVp9 = {
+		param( $commonParams, $outPrefix, $dir )
+		
+		Push-Location $dir
+		$logfile = "$env:temp\ffmpeg-vp9-" + (Get-Random) + ".log"
+		$vp9Params = "-passlogfile `"$logfile`"","-threads 8","-speed 1","-tile-columns 6","-frame-parallel 1","-auto-alt-ref 1","-lag-in-frames 25","-row-mt 1","-crf 33"
 
-	Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $mp4Params + "-pass 1" + "`"$($outPrefix).mp4`"")
-	Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $mp4Params + "-pass 2" + "`"$($outPrefix).mp4`"")
-}
+		Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $vp9Params + "-pass 1" + "`"$($outPrefix).webm`"")
+		Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $vp9Params + "-pass 2" + "`"$($outPrefix).webm`"")
+	}
 
-function encode-vp9 ( $commonParams, $outPrefix, $dir ){
-	Push-Location $dir
-	$logfile = "$env:temp\ffmpeg-vp9-" + (Get-Random) + ".log"
-	$vp9Params = "-passlogfile `"$logfile`"","-threads 8","-speed 1","-tile-columns 6","-frame-parallel 1","-auto-alt-ref 1","-lag-in-frames 25","-row-mt 1","-crf 33"
+	$encodeAv1 = {
+		param( $commonParams, $outPrefix, $dir )
+		Push-Location $dir
+		$logfile = "$env:temp\ffmpeg-av1-" + (Get-Random) + ".log"
+		$av1Params = "-c:v libaom-av1","-passlogfile `"$logfile`"","-row-mt 1","-tiles 2x2","-crf 30","-threads 8"
+		
+		Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $av1Params  + "-pass 1" + "`"$($outPrefix).webm`"")
+		Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $av1Params  + "-pass 2" + "`"$($outPrefix).webm`"")
+	}
 
-	Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $vp9Params + "-pass 1" + "`"$($outPrefix).webm`"")
-	Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $vp9Params + "-pass 2" + "`"$($outPrefix).webm`"")
-}
+	Start-Job $encodeMp4 -ArgumentList $commonParams,$outPrefix,$dir
+	# Start-Job $encodeVp9 -ArgumentList $commonParams,$outPrefix,$dir
+	Start-Job $encodeAv1 -ArgumentList $commonParams,$outPrefix,$dir
 
-function encode-av1 ( $commonParams, $outPrefix, $dir ){
-	Push-Location $dir
-	$logfile = "$env:temp\ffmpeg-av1-" + (Get-Random) + ".log"
-	$av1Params = "-c:v libaom-av1","-passlogfile `"$logfile`"","-threads 8","-row-mt 1","-tiles 2x2"
-	
-	Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $av1Params  + "-pass 1" + "`"$($outPrefix).webm`"")
-	Start-Process -wait -filepath "ffmpeg" -WindowStyle Minimized -ArgumentList ($commonParams + $av1Params  + "-pass 2" + "`"$($outPrefix).webm`"")
+	Get-Job | Receive-Job -wait -AutoRemoveJob
 }
 
 Encode $PSScriptRoot
